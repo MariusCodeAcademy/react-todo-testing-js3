f64e159e23f0388e856b805c358f7e8af7314a10
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-09-06 15:36:37 +0300

    prideti multiple todos uzduotis

--- src/components/Todo/__test__/Todo.test.js
+++ src/components/Todo/__test__/Todo.test.js
@@ -1,6 +1,40 @@
-// 1T
-// gaunam input,
-// ivedam reiksme
-// gauti mygtuka
-// paspaudziam mygtuka
-// expektinam rasti ivesta reiksme todo liste
+import { render, screen, fireEvent } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import Todo from '../Todo';
+
+const MockTodo = () => {
+  return (
+    <BrowserRouter>
+      <Todo />
+    </BrowserRouter>
+  );
+};
+
+function addToTodo(todoArr) {
+  // ivedam reiksme, paspaudziam mygtuka
+  // cikle
+}
+
+it('Should add single todo to a list', () => {
+  render(<MockTodo />);
+  // gaunam input,
+  const inputEl = screen.getByPlaceholderText(/Add a new task/);
+  // ivedam reiksme
+  fireEvent.change(inputEl, { target: { value: 'Do sports on Sunday' } });
+  // gauti mygtuka
+  const btnEl = screen.getByRole('button', { name: /add/i });
+  // paspaudziam mygtuka
+  fireEvent.click(btnEl);
+  const todoAddedEl = screen.getByText('Do sports on Sunday');
+  // expektinam rasti ivesta reiksme todo liste
+  // Assert
+  expect(todoAddedEl).toBeInTheDocument();
+  expect(todoAddedEl).toHaveClass('todo-item');
+});
+
+it('Should add multiple todos to a list', () => {
+  // taip pat kaip auksciau tik
+  // ivedam ir paspaudziam 3 k
+  // vietoj rankinio ivedimo, pasidaryti funkcija kuri gauna masyva
+  // ir ivedas masyvo el reiksme ir paspaudzia mygtuka
+});
