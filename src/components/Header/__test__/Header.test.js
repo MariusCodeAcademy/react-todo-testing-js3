2c45bb605f86d692d7f30edcbf8e0912f3eef9d6
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-09-06 11:59:05 +0300

    geting by text, title, id

---
+++ src/components/Header/__test__/Header.test.js
@@ -0,0 +1,34 @@
+import { render, screen } from '@testing-library/react';
+import Header from '../Header';
+
+test('should render text passed as props', () => {
+  render(<Header title='My Header' />);
+  const headerEl = screen.getByText(/my header/i);
+  expect(headerEl).toBeInTheDocument();
+});
+
+it('Header should be a heading with text', () => {
+  // Arrange
+  render(<Header title='My Header' />);
+  //
+
+  // Assert
+  const headingEl = screen.getByRole('heading', { name: 'My Header' });
+  expect(headingEl).toBeInTheDocument();
+});
+
+// pagal nematoma title
+it('Should have a title "second"', () => {
+  render(<Header title='My Header' />);
+
+  const otherHeding = screen.getByTitle('second');
+  expect(otherHeding).toBeInTheDocument();
+});
+
+// pagal tam skirta test id -- nerekomenduojama overuseinti
+it('Should render main title with id', () => {
+  render(<Header title='My Header' />);
+
+  const mainHeading = screen.getByTestId('main-title');
+  expect(mainHeading).toBeInTheDocument();
+});
