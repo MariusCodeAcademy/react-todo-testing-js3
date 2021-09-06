c95eacc814547422493ffeb184d0036e6f69282b
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-09-06 14:30:26 +0300

    Clear event tested

--- src/components/AddInput/__test__/AddInput.test.js
+++ src/components/AddInput/__test__/AddInput.test.js
@@ -21,7 +21,19 @@ describe('Input testing', () => {
     // Assert
     expect(inputEl.value).toBe('Do sports on Sunday');
   });
+
   it('Should clear input when adding todo', () => {
     // ar kai mes paspaudziam mygtuka inputas issivalo
+    // Arrange
+    render(<AddInput setTodos={mockFn} todos={[]} />);
+    const inputEl = screen.getByPlaceholderText(/Add a new task/);
+    // Act
+    // change(El, 'koks change')
+    fireEvent.change(inputEl, { target: { value: 'Do sports on Sunday' } });
+    //fireEvent.click(el) btnEl
+    const btnEl = screen.getByRole('button');
+    fireEvent.click(btnEl);
+    // Assert
+    expect(inputEl.value).toBe('');
   });
 });
