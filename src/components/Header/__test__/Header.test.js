91a05a2721d3a213a041b9da7fd67878bd623950
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-09-06 13:07:06 +0300

    header done move to footer

--- src/components/Header/__test__/Header.test.js
+++ src/components/Header/__test__/Header.test.js
@@ -1,34 +1,36 @@
 import { render, screen } from '@testing-library/react';
 import Header from '../Header';
 
-test('should render text passed as props', () => {
-  render(<Header title='My Header' />);
-  const headerEl = screen.getByText(/my header/i);
-  expect(headerEl).toBeInTheDocument();
-});
+describe('Header tests', () => {
+  test('should render text passed as props', () => {
+    render(<Header title='My Header' />);
+    const headerEl = screen.getByText(/my header/i);
+    expect(headerEl).toBeInTheDocument();
+  });
 
-it('Header should be a heading with text', () => {
-  // Arrange
-  render(<Header title='My Header' />);
-  //
+  it('Header should be a heading with text', () => {
+    // Arrange
+    render(<Header title='My Header' />);
+    //
 
-  // Assert
-  const headingEl = screen.getByRole('heading', { name: 'My Header' });
-  expect(headingEl).toBeInTheDocument();
-});
+    // Assert
+    const headingEl = screen.getByRole('heading', { name: 'My Header' });
+    expect(headingEl).toBeInTheDocument();
+  });
 
-// pagal nematoma title
-it('Should have a title "second"', () => {
-  render(<Header title='My Header' />);
+  // // pagal nematoma title
+  // it('Should have a title "second"', () => {
+  //   render(<Header title='My Header' />);
 
-  const otherHeding = screen.getByTitle('second');
-  expect(otherHeding).toBeInTheDocument();
-});
+  //   const otherHeding = screen.getByTitle('second');
+  //   expect(otherHeding).toBeInTheDocument();
+  // });
 
-// pagal tam skirta test id -- nerekomenduojama overuseinti
-it('Should render main title with id', () => {
-  render(<Header title='My Header' />);
+  // pagal tam skirta test id -- nerekomenduojama overuseinti
+  it('Should render main title with id', () => {
+    render(<Header title='My Header' />);
 
-  const mainHeading = screen.getByTestId('main-title');
-  expect(mainHeading).toBeInTheDocument();
+    const mainHeading = screen.getByTestId('main-title');
+    expect(mainHeading).toBeInTheDocument();
+  });
 });
