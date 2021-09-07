99297603eddb40ff8ff9a73d626a7748556cda42
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-09-07 11:02:51 +0300

    mock server using MSW

--- src/components/FollowersList/__test__/FollowersList.test.js
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -1,4 +1,8 @@
-import { screen, render } from '@testing-library/react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { render, waitFor, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
 import { BrowserRouter } from 'react-router-dom';
 import FollowersList from '../FollowersList';
 
@@ -9,21 +13,47 @@ const MockFList = () => {
     </BrowserRouter>
   );
 };
+const mockResponse = {
+  results: [
+    {
+      name: {
+        first: 'James',
+        last: 'Bond',
+      },
+      login: {
+        username: 'orangetiger284',
+      },
+      picture: {
+        large: 'https://randomuser.me/api/portraits/women/12.jpg',
+      },
+    },
+  ],
+};
+
+const server = setupServer(
+  rest.get('https://randomuser.me/api/', (req, res, ctx) => {
+    return res(ctx.json(mockResponse));
+  })
+);
+
+beforeAll(() => server.listen());
+afterEach(() => server.resetHandlers());
+afterAll(() => server.close());
 
 describe('Async testing', () => {
-  it('Renders Folowers on the screen', async () => {
-    render(<MockFList />);
-    // screen.debug();
-    const contactElArr = await screen.findAllByTestId(/contact-el/);
-    expect(contactElArr).not.toHaveLength(0);
-    expect(contactElArr).toHaveLength(1);
-    // expect(contactElArr).toBeInTheDocument();
-  });
+  // it('Renders Folowers on the screen', async () => {
+  //   render(<MockFList />);
+  //   // screen.debug();.//
+  //   const contactElArr = await screen.findAllByTestId(/contact-el/);
+  //   expect(contactElArr).not.toHaveLength(0);
+  //   expect(contactElArr).toHaveLength(1);
+  //   // expect(contactElArr).toBeInTheDocument();
+  // });
 
   it('Renders One Follower card on the screen', async () => {
     render(<MockFList />);
     const contactEl = await screen.findByTestId(/contact-el-0/);
     expect(contactEl).toBeInTheDocument();
-    screen.debug();
+    // screen.debug();
   });
 });
