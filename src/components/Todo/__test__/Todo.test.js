427971b1104e1bf69638f325c9f258b70490da8f
Marius Kuzminskas <marius.kuzminskas@ca.com>
2021-09-07 08:28:33 +0300

    Multiple add test, helper fn, notToHaveClass test

--- src/components/Todo/__test__/Todo.test.js
+++ src/components/Todo/__test__/Todo.test.js
@@ -12,19 +12,20 @@ const MockTodo = () => {
 
 function addToTodo(todoArr) {
   // ivedam reiksme, paspaudziam mygtuka
+  const inputEl = screen.getByPlaceholderText(/Add a new task/);
+  // gauti mygtuka
+  const btnEl = screen.getByRole('button', { name: /add/i });
   // cikle
+  todoArr.forEach((todo) => {
+    fireEvent.change(inputEl, { target: { value: todo } });
+    // paspaudziam mygtuka
+    fireEvent.click(btnEl);
+  });
 }
 
 it('Should add single todo to a list', () => {
   render(<MockTodo />);
-  // gaunam input,
-  const inputEl = screen.getByPlaceholderText(/Add a new task/);
-  // ivedam reiksme
-  fireEvent.change(inputEl, { target: { value: 'Do sports on Sunday' } });
-  // gauti mygtuka
-  const btnEl = screen.getByRole('button', { name: /add/i });
-  // paspaudziam mygtuka
-  fireEvent.click(btnEl);
+  addToTodo(['Do sports on Sunday']);
   const todoAddedEl = screen.getByText('Do sports on Sunday');
   // expektinam rasti ivesta reiksme todo liste
   // Assert
@@ -34,7 +35,35 @@ it('Should add single todo to a list', () => {
 
 it('Should add multiple todos to a list', () => {
   // taip pat kaip auksciau tik
+  render(<MockTodo />);
   // ivedam ir paspaudziam 3 k
   // vietoj rankinio ivedimo, pasidaryti funkcija kuri gauna masyva
   // ir ivedas masyvo el reiksme ir paspaudzia mygtuka
+  addToTodo(['one', 'two', 'three']);
+
+  const todoAddedArr = screen.getAllByTestId('todo-item');
+  // expektinam rasti ivesta reiksme todo liste
+  // Assert
+  expect(todoAddedArr).toHaveLength(3);
+});
+
+it('Task should not have completed class when added to list', () => {
+  // irasom reiksme
+  //mygtuko paspaudimo
+  //pridejomo i sarasa
+  // pasiziurim i klase
+  render(<MockTodo />);
+  addToTodo(['Do sports on Sunday']);
+  const todoAddedEl = screen.getByText('Do sports on Sunday');
+  // expektinam rasti ivesta reiksme todo liste
+  // Assert
+  expect(todoAddedEl).not.toHaveClass('todo-item-active');
+});
+
+test('Task should be gray when clicked(add a class)', () => {
+  // irasom reiksme
+  //mygtuko paspaudimo
+  //pridejomo i sarasa
+  // paspaudziam
+  // pasiziurim ar turi klase todo-item-active
 });
